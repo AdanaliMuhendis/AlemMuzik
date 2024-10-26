7085b677f36a83cf1642b18a4c7d4429322b6d62
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-10-26 17:42:13 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -28,24 +28,24 @@ for filename in os.listdir(r"./strings"):
 
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
-        languages["tr"] = yaml.safe_load(
-            open(r"./strings/langs/tr.yml", encoding="utf8")
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
         )
-        languages_present["tr"] = languages["tr"]["name"]
+        languages_present["en"] = languages["tr"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "tr":
+        if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["tr"]:
+        for item in languages["en"]:
             if item not in languages[language_name]:
-                languages[language_name][item] = languages["tr"][item]
+                languages[language_name][item] = languages["en"][item]
     try:
         languages_present[language_name] = languages[language_name]["name"]
     except:
         print(
-            "There is some issue with the language file inside bot. Please report it to the TheChampu at @TheChampuClub on Telegram"
+            "There is some issue with the language file inside bot. Please report it to the AdanaliMuhendis at @AlemSupport on Telegram"
         )
         sys.exit()
