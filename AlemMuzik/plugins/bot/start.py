9635f02fb67163167dad90d3821bc659bf5db181
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-10-28 15:06:31 +0300

    photo
    photo

--- AlemMuzik/plugins/bot/start.py
+++ AlemMuzik/plugins/bot/start.py
@@ -234,8 +234,9 @@ async def start_comm(client, message: Message, _):
 
         except AttributeError:
             chat_photo = "assets/nodp.png"
-        await message.reply_text(
-            text=_["start_2"].format(message.from_user.mention, app.mention),
+        await message.reply_photo(
+            photo=chat_photo,
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(config.LOG):
