fcca09c7a0d871ebb8087e09126b9e9cd9da83bd
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-10-28 15:11:57 +0300

    sasa
    sasa

--- AlemMuzik/plugins/play/playlist.py
+++ AlemMuzik/plugins/play/playlist.py
@@ -109,7 +109,7 @@ async def check_playlist(client, message: Message, _):
     else:
         # If more than the spam window time has passed, reset the command count and update the message timestamp
          user_command_count[user_id] = 1
-        user_last_message_time[user_id] = current_time
+         user_last_message_time[user_id] = current_time
 
     _playlist = await get_playlist_names(message.from_user.id)
     if _playlist:
